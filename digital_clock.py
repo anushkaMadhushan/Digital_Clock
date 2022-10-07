522d8e8ee66993486bf0acacad0d8b2894c0d690
Anushka <anushka@earrow.net>
2022-10-08 00:15:31 +0530

    Done

--- digital_clock.py
+++ digital_clock.py
@@ -1,3 +1,20 @@
+from cProfile import label
 from http.client import ImproperConnectionState
 from tkinter import *
 from time import strftime
+
+
+root = Tk()
+root.title("Digital Clock") 
+
+label = Label(root, font=("areal",160,"bold"), bg="black",fg="white")
+label.pack(anchor="center",fill="both",expand=1)
+
+
+def Time():
+    string = strftime("%H:%M:%S")
+    label.config(text=string)
+    label.after(1000,Time)
+    
+Time()
+root.mainloop()
